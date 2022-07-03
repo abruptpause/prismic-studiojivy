4c5677ce0e04f09be5482c028d0018f027d790d6
ixc7 <isaacxpreston7@gmail.com>
2022-07-03 15:01:02 -0400

    add eslint prettier/standard config

--- slices/Section/index.js
+++ slices/Section/index.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import { PrismicText } from '@prismicio/react'
 
+/*
+const Section = ({ children, bg = 'bg-slate-100', span }) => (
+  <div className={`min-h-screen grid grid-cols-12 ${bg}`}>
+    <div className={`${span} grid grid-cols-12 gap-14 m-14`}>{children}</div>
+  </div>
+)
+*/
+
+/*
+<Section span={span}>
+  <div className='relative col-span-6 aspect-4/5 self-start'>
+    <Image alt='' src='/images/main/JIV4.jpg' layout='fill' />
+  </div>
+  <div className='relative col-span-5 self-end'>
+    <Description />
+  </div>
+
+  <div className='relative col-span-6 self-end text-right'>
+    <Description />
+  </div>
+  <div className='relative col-span-5 aspect-square self-end'>
+    <Image alt='' src='/images/main/JIV5.jpg' layout='fill' />
+  </div>
+</Section>
+*/
+
 const Section = ({ slice }) => (
   <section>
-    { slice?.items?.map((item, i) => <img src={item.Image.url} alt={item.Image.alt} />) }
-    { slice?.items?.map((item, i) => <span>{item.Name}</span>) }
-    { slice?.items?.map((item, i) => <span >{ item.Start }</span>) }
-    { slice?.items?.map((item, i) => <span >{ item.End }</span>) }
+    {slice?.items?.map((item, i) => (
+      <img key={i} src={item.Image.url} alt={item.Image.alt} />
+    ))}
   </section>
 )
 
