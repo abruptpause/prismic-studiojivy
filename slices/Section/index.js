6c9a2b903f3d810f219569aab4a46bbdd7e144c9
ixc7 <isaacxpreston7@gmail.com>
2022-07-03 15:25:37 -0400

    update slice preview for 'Section'

--- slices/Section/index.js
+++ slices/Section/index.js
@@ -1,36 +1,25 @@
-import React from 'react'
-import { PrismicText } from '@prismicio/react'
-
-/*
-const Section = ({ children, bg = 'bg-slate-100', span }) => (
-  <div className={`min-h-screen grid grid-cols-12 ${bg}`}>
-    <div className={`${span} grid grid-cols-12 gap-14 m-14`}>{children}</div>
-  </div>
-)
-*/
-
-/*
-<Section span={span}>
-  <div className='relative col-span-6 aspect-4/5 self-start'>
-    <Image alt='' src='/images/main/JIV4.jpg' layout='fill' />
-  </div>
-  <div className='relative col-span-5 self-end'>
-    <Description />
-  </div>
-
-  <div className='relative col-span-6 self-end text-right'>
-    <Description />
-  </div>
-  <div className='relative col-span-5 aspect-square self-end'>
-    <Image alt='' src='/images/main/JIV5.jpg' layout='fill' />
-  </div>
-</Section>
-*/
+import Image from 'next/image'
 
 const Section = ({ slice }) => (
   <section>
-    {slice?.items?.map((item, i) => (
-      <img key={i} src={item.Image.url} alt={item.Image.alt} />
+    {slice?.items?.map((item, index) => (
+      <div key={index}>
+        <ul>
+          <li>name: {item.Name}</li>
+          <li>medium: {item.Medium}</li>
+          <li>size: {item.Size}</li>
+          <li>start: {item.Start}</li>
+          <li>end: {item.End}</li>
+          <li>aspect ratio X: {item.AspectRatioX}</li>
+          <li>aspect ratio Y: {item.AspectRatioY}</li>
+        </ul>
+        <Image
+          src={item.Image.url}
+          alt={item.Image.alt}
+          height='250'
+          width='250'
+        />
+      </div>
     ))}
   </section>
 )
